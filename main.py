c2f2422f6d65d7a9d13774864007755e50b2cec5
PaulLubnin <egopaul@inbox.ru>
2019-09-24 17:54:14 +0500

    написал метод подключения к вк

--- main.py
+++ main.py
@@ -10,14 +10,24 @@ class SessionConnection:
     Класс для создания подключения и запросов к ВК API, для сбора необходимых данных,
     которые будут записаны в базу для дальнейшей обработки
     """
-    def __init__(self):
-        pass
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+        self.vk_session = None
+        self.vk = None
 
     def login_vk(self):
         """
         Подключение к ВК по логину/паролю, либо токену
         """
-        pass
+        self.vk_session = vk_api.VkApi(self.login, self.password)
+        try:
+            self.vk_session.auth(token_only=False)
+            print('Подключение прошло успешно')
+        except vk_api.AuthError as error_msg:
+            print(error_msg)
+            return
+        self.vk = self.vk_session.get_api()
 
     def get_photos(self):
         """
@@ -107,4 +117,6 @@ class Selection:
 
 
 if __name__ == '__main__':
-    pass
+    connection = SessionConnection('', '')
+    connection.login_vk()
+
