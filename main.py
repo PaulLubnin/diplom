a17675d57859364537aa007ce2ad863a769ed745
PaulLubnin <egopaul@inbox.ru>
2019-10-02 12:54:37 +0500

    на этом этапе есть основа приложения без основных функций поиска и работы с базой

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import vk_api
+from pprint import pprint
 from pymongo import MongoClient
 import re
-
-ACCESS_TOKEN = ''
-APP_ID = 7145158
 
 class SessionConnection:
     """
@@ -16,9 +14,12 @@ class SessionConnection:
         self.vk_session = None
         self.vk = None
 
+        self.users = ''
+        self.fields = 'sex, bdate, home_town, interests, music, books'
+
     def login_vk(self):
         """
-        Подключение к ВК по логину/паролю, либо токену
+        Подключение к ВК по логину/паролю
         """
         self.vk_session = vk_api.VkApi(self.login, self.password)
         try:
@@ -29,35 +30,98 @@ class SessionConnection:
             return
         self.vk = self.vk_session.get_api()
 
-    def get_photos(self):
+class User(SessionConnection):
+    """
+    Пользователь приложение и методы для поиска на основе информации о пользователе
+    """
+    def get_application_user_information(self):
         """
-        Получить фотографии для записи в JSON
+        Получение и проверка информации о пользователе приложения
         """
-        pass
+        print('\n\tДля корректного поиска необходимо проверить Ваши данные:\n'
+              '\tдату рождения, город проживания и Ваши увлечения\n')
+        user_info = []
+        for info in self.vk.users.get(fields=self.fields):
+            if 'bdate' not in info.keys():
+                bdate = input('Укажите Вашу дату рождения в формате Д.М.Г: ')
+                info.update({'bdate': bdate})
+                print('Дата рождения принята')
+            else:
+                print('Дата рождения принята')
+            if len(info['home_town']) == 0:
+                home_town = input('Укажите Ваш город: ')
+                info.update({'home_town': home_town})
+                print('Город проживания принят')
+            else:
+                print('Город проживания принят')
+            if len(info['music']) == 0:
+                music = input('Любимые музыкальные группы (через запятую): ')
+                info.update({'music': music})
+                print('Музыкальные предпочтения учтены')
+            else:
+                print('Музыкальные предпочтения учтены')
+            if len(info['books']) == 0:
+                books = input('Любимые книги (через запятую): ')
+                info.update({'books': books})
+                print('Книжные предпочтения учтены')
+            else:
+                print('Книжные предпочтения учтены')
+            if len(info['interests']) == 0:
+                interests = input('Чем Вы увлекаетесь: ')
+                info.update({'interests': interests})
+                print('Ваши увлечения учтены')
+            else:
+                print('Ваши увлечения учтены')
+            info.update({'groups': self.vk.groups.get(user_id=info['id'])})
+            info.pop('can_access_closed')
+            info.pop('is_closed')
+            user_info = info
+        # pprint(user_info)
+        return user_info
 
-    def search_by_age(self):
+    def get_users(self):
         """
-        Поиск по возрасту
+        Получение списка пользователей ВК для пользователя приложения
         """
-        pass
+        print('Кого будем искть')
+        # input()
+        # sex =
+        # age_from =
+        # age_to =
+        self.users = self.vk.users.search(count=100, fields=self.fields, sex=1, hometown='Тюмень',
+                                          age_from=30, age_to=35)
+        # pprint(self.users)
+        return self.users
 
-    def search_by_gender(self):
-        """
-        Поиск по полу
-        """
-        pass
-
-    def group_search(self):
-        """
-        Поиск по группам
-        """
-        pass
-
-    def city_search(self):
-        """
-        Поиск по городу
-        """
-        pass
+    # def get_photos(self):
+    #     """
+    #     Получить фотографии для записи в JSON
+    #     """
+    #     pass
+    #
+    # def search_by_age(self):
+    #     """
+    #     Поиск по возрасту
+    #     """
+    #     pass
+    #
+    # def search_by_gender(self):
+    #     """
+    #     Поиск по полу
+    #     """
+    #     pass
+    #
+    # def group_search(self):
+    #     """
+    #     Поиск по группам
+    #     """
+    #     pass
+    #
+    # def city_search(self):
+    #     """
+    #     Поиск по городу
+    #     """
+    #     pass
 
 class DataBase:
     """
@@ -89,13 +153,14 @@ class DataBase:
 
 class Selection:
     """
-    Класс для отбора по заданным критериям
+    Отбор по музыке, книгам, интересам для пользователя приложения
     """
-    def __init__(self):
+    def __init__(self, for_whom_to_find):
         """
         Параметры для отбора
         """
-        pass
+        self.user = for_whom_to_find
+        return
 
     def selection_by_music(self):
         """
@@ -117,6 +182,10 @@ class Selection:
 
 
 if __name__ == '__main__':
-    connection = SessionConnection('', '')
-    connection.login_vk()
+    # connection = SessionConnection('', '')
+    # connection.login_vk()
+    user = User('', '')
+    user.login_vk()
+    user.get_application_user_information()
+    user.get_users()
 
