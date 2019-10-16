26340b593cd6d95b212d2b6d096e3069cb61aadd
PaulLubnin <egopaul@inbox.ru>
2019-10-16 18:40:44 +0500

    модуль готов

--- date_base/database.py
+++ date_base/database.py
@@ -14,17 +14,10 @@ class ClientDataBase:
         self.vkinder_db = self.client.vkinder_db
         self.users_collection = self.vkinder_db.users
 
-    def database_connection(self):
-        """
-         Подключение к базе
-        """
-        pass
-
     def data_loading(self, user_list):
         """
         Загрузка данных в базу
         """
-        print('Загрузка данных в базу...')
         self.users_collection.all_people.insert_many(user_list)
         print('...данные о пользователях загружены')
 
@@ -32,11 +25,10 @@ class ClientDataBase:
         """
         Получение данных из базы
         """
-        print('Загрузка данных из базы...')
         for found_users in self.users_collection.all_people.find():
             pprint(found_users)
         print('...данные о пользователях загружены')
 
     def drop_db(self):
         self.users_collection.all_people.drop()
-        print('База очищена')
+        print('...база очищена')
